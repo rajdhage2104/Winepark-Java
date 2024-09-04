d52c96a8d27032fb5f99614c16619ce693119b95
rajdhage2104 <144928213+rajdhage2104@users.noreply.github.com>
2024-09-04 10:31:45 +0530

    Update LambdaHandler.java

--- src/test/java/com/cybage/winepark/LambdaHandler.java
+++ src/test/java/com/cybage/winepark/LambdaHandler.java
@@ -1,14 +1,8 @@
-package com.cybage.winepark;
-
-import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.RequestHandler;
-
 public class LambdaHandler implements RequestHandler<Object, String> {
 
     @Override
     public String handleRequest(Object input, Context context) {
-        // Your Lambda function logic here
-        return "Hello, AWS Lambda!";
+        return "{\"message\": \"Hello, AWS Lambda!\"}";
     }
 }
 
